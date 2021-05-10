76a0e4cc2cfcd128db5003859b15fa458b68accb
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-10 14:42:06 -0400

    imported assertions

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace AssertionsImport
+{
+    public class handleAssertions {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void additionMethod() {
+            var first = 3;
+            var second = 7;
+            var result = first + second;
+
+            result.Should().Be(10);
+        }
+    }
+}
